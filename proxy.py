4dd171744110066a17d096891cfac4aa6ed0da7c
Yoga Pranata <ahmadzaylani001@gmail.com>
2023-10-18 05:58:26 +0700

    set max payload size according to vercel

--- proxy.py
+++ proxy.py
@@ -4,6 +4,7 @@ import httpx, uvicorn
 
 
 app = FastAPI()
+MAX_VERCEL_PAYLOAD = 4.5 * 1024 * 1024  # vercel maximum payload size
 
 
 @app.get("/")
@@ -25,6 +26,15 @@ async def proxied(url: str = None):
     try:
         async with httpx.AsyncClient(follow_redirects=True, verify=False) as client:
             response = await client.get(url)
+            if int(response.headers.get("Content-Length")) > int(MAX_VERCEL_PAYLOAD):  # this is for vercel, you can remove if not deploy on vercel
+                return JSONResponse(
+                    {
+                        "message": f"payload too large. max is {MAX_VERCEL_PAYLOAD} bytes, current payload is {response.headers.get("Content-Length")} bytes!",
+                        "success": False,
+                        "creator": "https://github.com/zYxDevs",
+                    },
+                    status_code=413,
+                )
             # response.raise_for_status()
             return Response(content=response.content, media_type=response.headers.get("Content-Type"), status_code=response.status_code)
     except Exception as e:
